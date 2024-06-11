6577ea5712b9cef9610583757cd2485b20c31d01
MoYiJiangNans <1545929126@qq.com>
2024-06-11 15:47:34 +0800

    对接高德API 获取地理位置和获取天气情况的API接口

--- src/views/PCVersion.vue
+++ src/views/PCVersion.vue
@@ -18,11 +18,15 @@
     <el-container class="container">
       <el-aside class="weatherPage">
         <div class="weatherConditions">
+          <div class="location">
+            <span class="province"> {{ locationData.province }} </span>
+            <span class="city"> {{ locationData.city }} </span>
+          </div>
           <div class="temperature">
-            <span>20°</span>
+            <span>{{ weatherData.temperature }}°</span>
           </div>
           <div class="maxAndMin">
-            <span>阴</span>
+            <span>{{ weatherData.weather }}</span>
             <span>最高22°</span>
             <span>最低16°</span>
           </div>
@@ -35,7 +39,22 @@
             </div>
           </div>
         </div>
+        <div>这里为测试窗口 后续开发完毕删除即可</div>
         <el-button type="primary" @click="toggleDivs">隐藏/显示</el-button>
+        <span>{{ locationData }}</span>
+        <div>天气数据</div>
+        地区编号：{{ weatherData.adcode }}
+        市区：{{ weatherData.city }}
+        空气湿度：{{ weatherData.humidity }}
+        空气湿度小数点：{{ weatherData.humidity_float }}
+        省：{{ weatherData.province }}
+        更新时间：{{ weatherData.reporttime }}
+        实时温度：{{ weatherData.temperature }}
+        实时温度小数点：{{ weatherData.temperature_float }}
+        天气：{{ weatherData.weather }}
+        风向：{{ weatherData.winddirection }}
+        风速：{{ weatherData.windpower }}
+
       </el-aside>
 
       <el-main class="dataPage">
@@ -104,15 +123,15 @@
 </template>
 
 <script setup lang="ts">
+import {onMounted, ref} from "vue";
 import {More, Plus} from "@element-plus/icons-vue";
 
-// 天气数据
 const tableData = [
   {
     time: '今天',
     weather: '多云',
     windDirection: '东北风二级',
-    Tag: '良',
+    tag: '良',
     weatherIcon: '多云图标',
     temperature: '18-25'
   },
@@ -120,23 +139,16 @@ const tableData = [
     time: '明天',
     weather: '小雨',
     windDirection: '西南风三级',
-    Tag: '轻度污染',
+    tag: '轻度污染',
     weatherIcon: '小雨图标',
     temperature: '15-20'
   },
-  {
-    time: '后天',
-    weather: '晴',
-    windDirection: '东风一级',
-    Tag: '优',
-    weatherIcon: '晴天图标',
-    temperature: '17-23'
-  },
+  {time: '后天', weather: '晴', windDirection: '东风一级', tag: '优', weatherIcon: '晴天图标', temperature: '17-23'},
   {
     time: '大后天',
     weather: '雷阵雨',
     windDirection: '南风四级',
-    Tag: '中度污染',
+    tag: '中度污染',
     weatherIcon: '雷阵雨图标',
     temperature: '20-24'
   },
@@ -144,11 +156,102 @@ const tableData = [
     time: '周六',
     weather: '阴转小雨',
     windDirection: '西北风二级',
-    Tag: '良',
+    tag: '良',
     weatherIcon: '阴转小雨图标',
     temperature: '14-21'
   }
-]
+];
+
+// 位置信息
+let locationData = ref({
+  status: '',
+  info: '',
+  infocode: '',
+  province: '',
+  city: '',
+  adcode: '',
+  rectangle: ''
+});
+
+// 天气信息
+let weatherData = ref({
+  adcode: "",
+  city: "",
+  humidity: "",
+  humidity_float: "",
+  province: "",
+  reporttime: "",
+  temperature: "",
+  temperature_float: "",
+  weather: "",
+  winddirection: "",
+  windpower: ""
+});
+
+const apiKey = import.meta.env.VITE_AMAP_API_KEY;
+
+const getLocation = async () => {
+  try {
+    const response = await fetch(`https://restapi.amap.com/v3/ip?key=${apiKey}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    locationData.value = {
+      status: data.status || 'NULL',
+      info: data.info || 'NULL',
+      infocode: data.infocode || 'NULL',
+      province: data.province || 'XX省',
+      city: data.city || 'XX市',
+      adcode: data.adcode || 'NULL',
+      rectangle: data.rectangle || 'NULL'
+    };
+
+    console.log(locationData.value); // 在此打印 locationData 确认 adcode
+  } catch (error) {
+    console.error('Error:',);
+  }
+};
+
+
+const getWeather = async (adcode: string) => {
+  if (!adcode) {
+    console.error('adcode is empty');
+    return;
+  }
+
+  try {
+    const response = await fetch(`https://restapi.amap.com/v3/weather/weatherInfo?city=${adcode}&key=${apiKey}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const dataJson = await response.json();
+    const data = dataJson.lives[0];
+    weatherData.value = {
+      adcode: data.status || 'NULL',
+      city: data.city || 'XX市',
+      humidity: data.humidity || 'NULL',
+      humidity_float: data.humidity_float || 'NULL',
+      province: data.province || 'XX省',
+      reporttime: data.reporttime || 'NULL',
+      temperature: data.temperature || 'NULL',
+      temperature_float: data.temperature_float || 'NULL',
+      weather: data.weather || 'NULL',
+      winddirection: data.winddirection || 'NULL',
+      windpower: data.windpower || 'NULL',
+    }
+  } catch (error) {
+    console.error('Error:',);
+  }
+};
+
+onMounted(async () => {
+  // 先让getLocation执行
+  await getLocation();
+  // 获取天气情况（参数是地区编号） -> 安徽省 宿州市
+  getWeather(locationData.value.adcode);
+});
+
 
 const toggleDivs = () => {
   const warningDiv = document.querySelector('.warning') as HTMLElement;
@@ -157,9 +260,9 @@ const toggleDivs = () => {
     warningDiv.style.display = warningDiv.style.display === 'none' ? 'block' : 'none';
     rainChartDiv.style.display = rainChartDiv.style.display === 'none' ? 'block' : 'none';
   }
-}
-
+};
 </script>
+
 
 <style scoped>
 * {
@@ -234,13 +337,19 @@ const toggleDivs = () => {
   padding: 10px;
   margin: 0 0 20px 20px;
 }
+
 .el-table {
-  margin: 10px 0 0 0 ;
+  margin: 10px 0 0 0;
   --el-table-border-color: none;
   --el-table-bg-color: transparent;
   --el-table-tr-bg-color: transparent;
   --el-table-text-color: #fff;
-  --el-table-row-hover-bg-color: rgba(255,255,255,0.2);
+  --el-table-row-hover-bg-color: rgba(255, 255, 255, 0.2);
+  border-radius: 10px;
+}
+
+.el-table__row {
+  border-radius: 10px;
 }
 
 .weatherConditions {
@@ -264,7 +373,7 @@ const toggleDivs = () => {
   font-size: 15px;
   width: 27%;
   border-radius: 10px;
-  background-color: rgba(255,255,255,0.1);
+  background-color: rgba(255, 255, 255, 0.1);
   padding: 10px;
   margin: 0 10px 0 0;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import dotenv from 'dotenv';
+
+// 加载 .env 文件
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +24,6 @@ export default defineConfig({
                     ElementPlusResolver()
                 ],
             }
-        )
+        ),
     ],
 })
